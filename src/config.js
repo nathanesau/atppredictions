44158db799d4431a4049baa1316887170271289f
Nathan Esau <nathanesau1@gmail.com>
2023-12-29 23:21:39 -0800

    move variables to secrets

--- src/config.js
+++ src/config.js
@@ -1,12 +1,9 @@
 export function getConfig() {
-  // TODO: manage this via secrets
   const audience = "atppredictions";
 
-  // TODO: manage this via secrets
-  const domain = "dev-e7x276delr7bwmo8.us.auth0.com";
+  const domain = process.env.DOMAIN;
 
-  // TODO: manage this via secrets
-  const clientId = "IQ2txbSiLA50jhKgS42iNAgz4wTL0Sq7"
+  const clientId = process.env.CLIENT_ID;
 
   return {
     domain: domain,
