18778fe1ad13152a8f86a1b238b51092961869f0
Nathan Esau <nathanesau1@gmail.com>
2023-12-29 23:30:12 -0800

    Revert "move variables to secrets"
    This reverts commit 44158db799d4431a4049baa1316887170271289f.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 export function getConfig() {
+  // TODO: manage this via secrets
   const audience = "atppredictions";
 
-  const domain = process.env.DOMAIN;
+  // TODO: manage this via secrets
+  const domain = "dev-e7x276delr7bwmo8.us.auth0.com";
 
-  const clientId = process.env.CLIENT_ID;
+  // TODO: manage this via secrets
+  const clientId = "IQ2txbSiLA50jhKgS42iNAgz4wTL0Sq7"
 
   return {
     domain: domain,
