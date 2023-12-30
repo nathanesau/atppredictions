2977ebf9a8c6e102a90b491283de210d7b39e3b7
Nathan Esau <nathanesau1@gmail.com>
2023-12-29 21:50:10 -0800

    add placeholder values for auth0

--- src/config.js
+++ src/config.js
@@ -1,20 +1,16 @@
-import configJson from "./auth_config.json";
-
 export function getConfig() {
-  // Configure the audience here. By default, it will take whatever is in the config
-  // (specified by the `audience` key) unless it's the default value of "YOUR_API_IDENTIFIER" (which
-  // is what you get sometimes by using the Auth0 sample download tool from the quickstart page, if you
-  // don't have an API).
-  // If this resolves to `null`, the API page changes to show some helpful info about what to do
-  // with the audience.
-  const audience =
-    configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
-      ? configJson.audience
-      : null;
+  // TODO: manage this via secrets
+  const audience = "atppredictions";
+
+  // TODO: manage this via secrets
+  const domain = "dev-e7x276delr7bwmo8.us.auth0.com";
+
+  // TODO: manage this via secrets
+  const clientId = "IQ2txbSiLA50jhKgS42iNAgz4wTL0Sq7"
 
   return {
-    domain: configJson.domain,
-    clientId: configJson.clientId,
+    domain: domain,
+    clientId: clientId,
     ...(audience ? { audience } : null),
   };
 }
